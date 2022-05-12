1eab20f34a038a01556102e494dbe6067a61a96d
LyokoJeremie <lucheng989898@gmail.com>
2022-05-12 18:32:56 +0800

    test code

---
+++ test2.py
@@ -0,0 +1,42 @@
+from time import sleep
+
+from src.FH0A import get_airplane_manager
+from src.FH0A.AirplaneManagerAdapter import AirplaneManager
+from src.FH0A.AirplaneManagerAdapter import AirplaneController
+
+if __name__ == '__main__':
+    m: AirplaneManager = get_airplane_manager()
+
+    # print('airplanes_table', m.airplanes_table)
+
+    m.flush()
+
+    # print('airplanes_table', m.airplanes_table)
+
+    print(m.start())
+
+    a = m.get_airplane_extended('COM3')
+
+    a.takeoff(100)
+    sleep(5)
+
+    # a.left(50)
+    # sleep(5)
+    # a.right(50)
+    # sleep(5)
+    #
+    # a.forward(100)
+    # sleep(5)
+    # a.back(100)
+    # sleep(5)
+
+    a.cw(100)
+    sleep(8)
+    a.ccw(100)
+    sleep(8)
+
+    a.land()
+    sleep(2)
+
+    a.shutdown()
+    exit(0)
