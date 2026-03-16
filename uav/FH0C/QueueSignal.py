d5549ee3abcd1180f09a8c93a77230f7757a3d24
LyokoJeremie <lucheng989898@gmail.com>
2026-03-16 15:44:40 +0800

    QueueSignal

--- uav/FH0C/QueueSignal.py
+++ uav/FH0C/QueueSignal.py
@@ -2,6 +2,10 @@ from enum import Enum
 
 
 class QueueSignal(Enum):
+    # stop send
     SHUTDOWN = 0
+    # send a command
     CMD = 1
+    # clean last send command (to stop re-send logic)
+    CLEAN = 1
     pass
