56c343b3286d25c2b9afde2f75bdab15fa929673
LyokoJeremie <lucheng989898@gmail.com>
2026-04-26 19:50:48 +0800

    .

--- uav/FH0C/image_process.py
+++ uav/FH0C/image_process.py
@@ -7,7 +7,12 @@ def image_file_2_mat(imageFileData: bytes):
     img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
     return img
 
-def write_mat_2_file(m: np.ndarray, path: str):
+def write_mat_2_file(m, path: str):
+    """
+    :param m: jpg formatted image data
+    :param path: 
+    :return:
+    """
     im_arr = np.frombuffer(m, dtype=np.uint8)
     cv2.imwrite(path, im_arr)
     print(f"write_mat_2_file Image saved to {path}")
